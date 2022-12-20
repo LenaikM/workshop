211d84529bd8837052ce8de4bd16d46eeb0aa21e
LenaikM <lenaik_mohamadi@outlook.fr>
2022-12-20 09:35:50 +0100

    nouveau dossier angular

---
+++ src/main/java/com/workshop/workshop/service/ConnexionController.java
@@ -0,0 +1,30 @@
+package com.workshop.workshop.service;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.security.core.Authentication;
+
+@Controller
+public class ConnexionController {
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        return "well";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/welcome")
+    public String welcomePage(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("username", auth.getPrincipal());
+
+        return "welcome";
+    }
+}
