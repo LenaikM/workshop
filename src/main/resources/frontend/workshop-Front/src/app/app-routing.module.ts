20a7267861bcf6d7c1b308e0e6db822eb6fb19ea
LenaikM <lenaik_mohamadi@outlook.fr>
2022-12-23 16:16:05 +0100

    modifs composants accueil et abonnement

--- src/main/resources/frontend/workshop-Front/src/app/app-routing.module.ts
+++ src/main/resources/frontend/workshop-Front/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UserFormComponent} from "./user-form/user-form.component";
 import {UserListComponent} from "./user-list/user-list.component";
+import {AccueilSalleComponent} from "./accueil-salle/accueil-salle.component";
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
-  { path: 'adduser', component: UserFormComponent }
+  { path: 'adduser', component: UserFormComponent },
+  { path: 'accueil', component: AccueilSalleComponent }
 ];
 
 @NgModule({
